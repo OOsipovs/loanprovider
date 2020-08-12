62796c8722188fc6b32656d12d86ca2641ddbcfe
Olegs <olegs.osipovs@gmail.com>
2020-08-12 19:48:27 +0300

    Transformin loan request

--- src/main/java/com/peerlender/lendingengine/domain/repository/LoanApplicationRepository.java
+++ src/main/java/com/peerlender/lendingengine/domain/repository/LoanApplicationRepository.java
@@ -3,7 +3,7 @@ package com.peerlender.lendingengine.domain.repository;
 import com.peerlender.lendingengine.domain.model.LoanApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LoanRequestRepository extends JpaRepository<LoanApplication, Long> {
+public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
 
 
 }
